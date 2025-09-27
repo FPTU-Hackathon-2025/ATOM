abe6b4d8b577b846e2e71a08c0c9ba1025a02686
kaiquincy <kawdwk@gmail.com>
2025-09-27 22:08:09 +0700

    av

--- src/ros_lidar_follower.py
+++ src/ros_lidar_follower.py
@@ -281,8 +281,8 @@ class JetBotController:
 
     def setup_parameters(self):
         self.WIDTH, self.HEIGHT = 300, 300
-        self.BASE_SPEED = 0.16
-        self.TURN_SPEED = 0.18
+        self.BASE_SPEED = 0.15
+        self.TURN_SPEED = 0.17
         self.TURN_DURATION_90_DEG = 0.8
         self.ROI_Y = int(self.HEIGHT * 0.85)
         self.ROI_H = int(self.HEIGHT * 0.15)
